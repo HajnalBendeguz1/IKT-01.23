ba7949f9d909e5b394fa410062132ce2c432449f
Hajnal Bendegúz <bendeguzhajnal76@gmail.com>
2023-03-06 10:14:04 +0100

    b  nb

--- index-angol.php
+++ index-angol.php
@@ -6,49 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="zaszlok.css">
     <style>
-      .div1 {
-        text-align: center;
-      }
-      .div2,
-      .div3,
-      .div4,
-      .div5,
-      .div6,
-      .div7 {
-        display: inline-block;
-        margin: 10px;
-        
-      }
-      body{
-        background-color: grey;
-      }
-  .div1 {
-    text-align: center;
-    color: white;
-  }
-  .img {
-    height: 200px;
-    width: 200px;
-    margin: 20px;
-    border: 2px solid black;
-    box-shadow: 5px 5px 10px black;
-    transition: transform 0.3s;
-  }
-  .img:hover {
-    transform: scale(1.2);
-  }
-  h2 {
-    font-family: Arial, sans-serif;
-    text-shadow: 2px 2px 10px black;
-  }
-  body{
-      font-family: Arial, sans-serif;
-			margin: 0;
-			padding: 0;
-        }
-        body{
-            background: linear-gradient(to left, #ff0000, #000000);
-        }
+  body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  background: linear-gradient(to left, #ff0000, #000000);
+}
+
+.img {
+  height: 200px;
+  width: 200px;
+  margin: 20px;
+  border: 2px solid black;
+  box-shadow: 5px 5px 10px black;
+  transition: transform 0.3s;
+}
+
+.img:hover {
+  transform: scale(1.2);
+}
+
+h2 {
+  font-family: Arial, sans-serif;
+  text-shadow: 2px 2px 10px black;
+  color: white;
+  text-align: center;
+}
+
+.division {
+  display: inline-block;
+  margin: 10px;
+}
+
+.division:hover {
+  transform: rotate(10deg);
+  transition: transform 0.5s ease-out;
+}
     </style>
   </head>
   <body>
@@ -56,36 +49,36 @@
     <?php $header = file_get_contents("./header-angol.html"); echo $header; ?>
   <!-- ######### HEADER ######### -->
     <div><a href="index.php"><img src="mzaszlo.png" alt="Magyar" title="Magyar" class="zaszlok"></a></div>
-    <div class="div2">
+    <div class="division">
       <a href="információ angol.html">
         <img src="d.jpeg" alt="information gathering" title="information gathering" class="img">
       </a>
     </div>
-    <div class="div3">
+    <div class="division">
       <a href="harver-angol.html">
         <img src="retzui.jpg" alt="hardver" title="hardver" class="img">
       </a>
     </div>
-    <div class="div4">
+    <div class="division">
       <a href="topológia-angol.html">
         <img src="topology1.png" alt="topology" title="topology" class="img">
       </a>
     </div>
-    <div class="div5">
+    <div class="division">
       <a href="dokumentáció-angol.html">
         <img src="unnamed.png" alt="documentation" title="documentation" class="img">
       </a>
     </div>
-    <div class="div6">
+    <div class="division">
       <a href="karbantartás-angol.html">
         <img src="a.png" alt="maintenance" title="maintenance" class="img">
       </a>
     </div>
-    <div class="div7">
+    <div class="division">
       <a href="tervezés-angol.html">
         <img src="terv.png" alt="plan" title="plan" class="img">
       </a>
     </div>
-    <div class="div1">
+    <div>
       <h2>
         Hello, we are the founders of .... We study part-time at the Széchenyi István Székesfehérvár SZC Széchenyi István Technical Technical College and we are interns at Harman as system operators. Our company deals with various complex simulations, conducting scientific research on astronomical entities (e.g. Supernovae, Nebulae). Although we work with rather few people, we are very efficient. We complement each other's work, we help each other, we solve problems together.</h2>
--- index.php
+++ index.php
@@ -6,26 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="zaszlok.css">
     <style>
-      .div1 {
-        text-align: center;
-      }
-      .div2,
-      .div3,
-      .div4,
-      .div5,
-      .div6,
-      .div7 {
-        display: inline-block;
-        margin: 10px;
-        
-      }
-      body{
-        background-color: grey;
-      }
-  .div1 {
-    text-align: center;
-    color: white;
-  }
   body {
   margin: 0;
   padding: 0;
@@ -33,38 +13,6 @@
   background: linear-gradient(to left, #ff0000, #000000);
 }
 
-/* Header styles */
-.header {
-  background-color: #333;
-  color: white;
-  padding: 20px;
-  text-align: center;
-}
-
-/* Navigation styles */
-.navbar {
-  background-color: #555;
-  overflow: hidden;
-}
-
-.navbar a {
-  float: left;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-.navbar a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.active {
-  background-color: #4CAF50;
-}
-
-/* Image styles */
 .img {
   height: 200px;
   width: 200px;
@@ -78,7 +26,6 @@
   transform: scale(1.2);
 }
 
-/* Text styles */
 h2 {
   font-family: Arial, sans-serif;
   text-shadow: 2px 2px 10px black;
@@ -86,39 +33,11 @@ h2 {
   text-align: center;
 }
 
-/* Division styles */
 .division {
   display: inline-block;
   margin: 10px;
 }
 
-/* Gradient button styles */
-.gradient-button {
-  display: inline-block;
-  padding: 10px 20px;
-  font-size: 16px;
-  text-align: center;
-  text-decoration: none;
-  border-radius: 5px;
-  color: white;
-  background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
-  box-shadow: 0px 5px 0px 0px #3e8e41;
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-}
-
-.gradient-button:hover {
-  background-position: right center;
-  box-shadow: 0px 5px 0px 0px #3e8e41;
-  transform: translateY(-4px);
-}
-
-.gradient-button:active {
-  background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
-  box-shadow: 0px 2px 0px 0px #3e8e41;
-  transform: translateY(2px);
-}
-
-/* Hover effect on division */
 .division:hover {
   transform: rotate(10deg);
   transition: transform 0.5s ease-out;
@@ -128,36 +47,36 @@ h2 {
   <body>
   <?php $header = file_get_contents("./header.html"); echo $header;?>
     <div><a href="index-angol.php"><img src="azaszlo.png" alt="English" title="English" class="zaszlok"></a></div>
-    <div class="div2">
-      <a href="információ.php" class="gradient-button">
+    <div class="division">
+      <a href="információ.php">
         <img src="d.jpeg" alt="informácó gyűjtés" title="információ gyűjtés" class="img">
       </a>
     </div>
-    <div class="div3">
-      <a href="harver.php" class="gradient-button">
+    <div class="division">
+      <a href="harver.php">
         <img src="retzui.jpg" alt="hardver" title="hardver" class="img">
       </a>
     </div>
-    <div class="div4">
-      <a href="topológia.php" class="gradient-button">
+    <div class="division">
+      <a href="topológia.php">
         <img src="topology1.png" alt="topológia" title="topológia" class="img">
       </a>
     </div>
-    <div class="div5">
-      <a href="dokumentáció.php" class="gradient-button">
+    <div class="division">
+      <a href="dokumentáció.php">
         <img src="unnamed.png" alt="dokumentáció" title="dokumentáció" class="img">
       </a>
     </div>
-    <div class="div6">
-      <a href="karbantartás.php" class="gradient-button">
+    <div class="division">
+      <a href="karbantartás.php">
         <img src="a.png" alt="karbantartás" title="karbantartás" class="img">
       </a>
     </div>
-    <div class="div7">
-      <a href="tervezés.php" class="gradient-button">
+    <div class="division">
+      <a href="tervezés.php">
         <img src="terv.png" alt="tervrajz" title="tervrajz" class="img">
       </a>
     </div>
-    <div class="div1" class="division">
+    <div >
       <h2>
         Helló, mi vagyunk a … nevű cég alapítói. Másodállásban a Székesfehérvári SZC Széchenyi István Műszaki Technikumban tanulunk, valamint gyakorlaton vagyunk a Harmannál, mint rendszerüzemeltetők. Cégünk különböző komplex szimulációkkal foglalkozik, tudományos kutatásokat végez asztronómiai entitásokról (pl. Szupernóvák, Nebulák). Bár meglehetősen kevés emberrel dolgozunk, nagyon hatékonyak vagyunk. Kiegészítjük egymás munkáját, segítünk egymásnak, közösen oldjuk meg a problémákat.</h2>
